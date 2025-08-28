a90cf88aa67c51e86f7aa3f2799004ef52f4d4af
Tiana1312 <odeyemisheyifunmi@gmail.com>
2025-08-28 20:26:20 +0100

    initial commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,25 +1,2 @@
 export * from "./config";
 
-// import * as dotenv from "dotenv";
-// dotenv.config({path: ".env"})
-
-// const db = {
-//         type: "postgres" as const,
-//         host: process.env.DB_HOST || "localhost",
-//         port: parseInt(process.env.DB_PORT || "5432"),
-//         username: process.env.DB_USERNAME || "",
-//         password: process.env.DB_PASSWORD || "",
-//         database: process.env.DB_NAME || "", 
-// }
-    
-// const env = {
-//     isProd: process.env.NODE_ENV === "production",
-// }
-
-// const app = {
-//     port: process.env.PORT || 3000
-// }
-
-// export default {
-//     db, env, app
-// }
